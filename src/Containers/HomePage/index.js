e1472ba44d3b010bc34546778f8b06d9504ecfb9
incredible5 <mauryachaitany@gmail.com>
2021-10-26 01:48:00 +0530

    Add search functionality

--- src/Containers/HomePage/index.js
+++ src/Containers/HomePage/index.js
@@ -6,21 +6,41 @@ import "./style.scss";
 
 function Homepage(props) {
   const [showChart, setShowChart] = useState(false);
+  const [dataCopy, setDataCopy] = useState([]);
+  const [data, setData] = useState([]);
 
-  let data = localStorage.getItem("data") || [];
-  if (data) {
-    data = JSON.parse(data);
+  let tableData = localStorage.getItem("data");
+  if (tableData && data.length === 0) {
+    tableData = JSON.parse(tableData);
+    tableData = tableData.reduce((acc, curr, idx) => {
+      acc.push({
+        id: idx,
+        data: curr,
+      });
+      return acc;
+    }, []);
+    setData(tableData);
+    setDataCopy(tableData);
   }
+
+  const filterData = (searchStr) => {
+    let modifiedData = dataCopy.filter((item) =>
+      item.data.find(
+        (d) => d.toLowerCase().indexOf(searchStr.toLowerCase()) >= 0
+      )
+    );
+    setData(modifiedData);
+  };
 
   const renderDataTable = (rowData, idx) => {
     return (
       <tr key={idx}>
-        {rowData.map((item, index) => {
+        {rowData.data.map((item, index) => {
           return (
             <td
               key={index}
               onClick={() => {
-                window.location.href = "/details/" + idx;
+                window.location.href = "/details/" + rowData.id;
               }}
             >
               {item}
@@ -38,6 +58,12 @@ function Homepage(props) {
       </div>
       <div className={"chart-prev-btn"} onClick={() => setShowChart(true)}>
         Show top 10 employees salary data
+      </div>
+      <div className={"search-sec"}>
+        <input
+          placeholder="Search any column value. Eg: Specialist"
+          onChange={(e) => filterData(e.target.value)}
+        />
       </div>
       <table className={"data-table"}>
         <thead>
